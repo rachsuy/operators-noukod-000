0014e6f5f8d728360bccdc1183111bbdc26d5f67
Rachele Suy <rachelesuy@gmail.com>
2019-04-23 08:55:04 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,19 @@
 def unsafe?(speed)
-
+  if speed < 40
+   return true
+  elsif speed > 60
+  return true
+ else 
+  return false 
+  
+ end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed <  true : false
+  speed > 60 true : false
 end
 	
 
